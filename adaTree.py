7ad9590ddef4ae0bd947736cbcba293f448d6d00
Kurt Thomas <kurt.thomas34@gmail.com>
2022-09-20 14:25:16 -0400

    formatting and f strings (#1)
    * convert to f strings
    
    * update gitignore
    
    * run black without string norm
    
    Co-authored-by: Kurt M. Thomas <kurt@gamesensesports.com>

--- adaTree.py
+++ adaTree.py
@@ -18,7 +18,9 @@ week = 1
 
 readPath = './cleaned.csv'
 
-data: df = pd.read_csv(readPath,)
+data: df = pd.read_csv(
+    readPath,
+)
 data = data.sort_values(['year', 'week'])
 
 
@@ -30,14 +32,38 @@ def showIf():
 # first 10 games for each team blank (10 x 32) but divide by 2 because we only record 1 instance of each matchup
 data = data.iloc[160:]
 
-x_cols = ['Home_Fav', 'Home_Vegas_Spread', 'Trail_Home_Score', 'Trail_Away_Score', 'Home_Allowed',
-          'Away_Allowed', 'Home_TO', 'Away_TO', 'Home_FTO', 'Away_FTO',
-          'Home_Pass_Eff', 'Away_Pass_Eff', 'Home_Pass_Def', 'Away_Pass_Def',
-          'Home_Rush_Eff', 'Away_Rush_Eff', 'Home_Rush_Def', 'Away_Rush_Def',
-          'Home_Pen_Yds', 'Away_Pen_Yds', 'Home_Pen_Yds_Agg', 'Away_Pen_Yds_Agg',
-          'Home_Third_Eff', 'Away_Third_Eff', 'Home_Third_Def', 'Away_Third_Def',
-          'Home_Fourth_Eff', 'Away_Fourth_Eff', 'Home_Fourth_Def',
-          'Away_Fourth_Def']
+x_cols = [
+    'Home_Fav',
+    'Home_Vegas_Spread',
+    'Trail_Home_Score',
+    'Trail_Away_Score',
+    'Home_Allowed',
+    'Away_Allowed',
+    'Home_TO',
+    'Away_TO',
+    'Home_FTO',
+    'Away_FTO',
+    'Home_Pass_Eff',
+    'Away_Pass_Eff',
+    'Home_Pass_Def',
+    'Away_Pass_Def',
+    'Home_Rush_Eff',
+    'Away_Rush_Eff',
+    'Home_Rush_Def',
+    'Away_Rush_Def',
+    'Home_Pen_Yds',
+    'Away_Pen_Yds',
+    'Home_Pen_Yds_Agg',
+    'Away_Pen_Yds_Agg',
+    'Home_Third_Eff',
+    'Away_Third_Eff',
+    'Home_Third_Def',
+    'Away_Third_Def',
+    'Home_Fourth_Eff',
+    'Away_Fourth_Eff',
+    'Home_Fourth_Def',
+    'Away_Fourth_Def',
+]
 
 train = data[data['year'] < 2022]
 X = train[x_cols]
@@ -45,7 +71,8 @@ y = train['Home_Actual_Spread']
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=.2, random_state=88)
+    X, y, test_size=0.2, random_state=88
+)
 
 
 if runGridSearch:
@@ -61,16 +88,22 @@ if runGridSearch:
         'n_estimators': [i for i in range(1600, 2000, 25)],
         'learning_rate': [0.00001, 0.0001, 0.001],
         'loss': ['linear'],  # , 'square', 'exponential'],
-        'random_state': [88]
+        'random_state': [88],
     }
 
     ada = AdaBoostRegressor(base_estimator=DecisionTreeRegressor())
 
-    clf = GridSearchCV(ada, parameters, verbose=3,
-                       scoring='neg_root_mean_squared_error', n_jobs=12)
+    clf = GridSearchCV(
+        ada, parameters, verbose=3, scoring='neg_root_mean_squared_error', n_jobs=12
+    )
     estimator = clf.fit(X_train, y_train)
-    resultDf = pd.concat([pd.DataFrame(clf.cv_results_['params']), pd.DataFrame(
-        clf.cv_results_['mean_test_score'])], axis=1)
+    resultDf = pd.concat(
+        [
+            pd.DataFrame(clf.cv_results_['params']),
+            pd.DataFrame(clf.cv_results_['mean_test_score']),
+        ],
+        axis=1,
+    )
     resultDf.to_csv(gridSearchResultPath)
     print('best estimator: ', clf.best_estimator_)
     print('best params: ', clf.best_params_)
@@ -79,11 +112,18 @@ if runGridSearch:
 
 # After doing grid search, put best parameters here
 else:
-    predictionResultPath = Path('predictions/adaTree_week_{}.csv'.format(week))
+    predictionResultPath = Path(f'predictions/adaTree_week_{week}.csv')
     predictionResultPath.parent.mkdir(parents=True, exist_ok=True)
 
-    regr = make_pipeline(AdaBoostRegressor(DecisionTreeRegressor(
-        max_depth=15, max_features='sqrt'), n_estimators=1800, learning_rate=0.0001, loss='linear', random_state=88))
+    regr = make_pipeline(
+        AdaBoostRegressor(
+            DecisionTreeRegressor(max_depth=15, max_features='sqrt'),
+            n_estimators=1800,
+            learning_rate=0.0001,
+            loss='linear',
+            random_state=88,
+        )
+    )
 
     estimator = regr.fit(X_train, y_train)
     y_val_pred = regr.predict(X_test)
@@ -98,9 +138,11 @@ else:
     X_test = test[x_cols]
 
     estimator = regr.fit(X_train, y_train)
-    y_test_pred = pd.DataFrame(regr.predict(
-        X_test), columns=['Predicted Spread'])
+    y_test_pred = pd.DataFrame(regr.predict(X_test), columns=['Predicted Spread'])
 
-    predictions = test[['Home_Team', 'Away_Team',
-                        'Home_Vegas_Spread']].reset_index(drop=True).join(y_test_pred['Predicted Spread'].reset_index(drop=True))
+    predictions = (
+        test[['Home_Team', 'Away_Team', 'Home_Vegas_Spread']]
+        .reset_index(drop=True)
+        .join(y_test_pred['Predicted Spread'].reset_index(drop=True))
+    )
     predictions.to_csv(predictionResultPath)
