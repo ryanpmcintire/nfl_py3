196ecec06502f7ca271b645b36e249da1a892c90
Ryan McIntire <ryanpmcintire@gmail.com>
2022-10-20 10:46:07 -0400

    stash

--- modules/rank.py
+++ modules/rank.py
@@ -6,32 +6,38 @@ import operator
 
 from cleaner import clean
 
-read_path = './cleaned.csv'
+read_path = './cleaned_with_dupes.csv'
 
 
 
 def empty_grid():
   return pd.DataFrame(np.zeros((32, 32)), columns=config.FRANCHISE_ABBRV, index=config.FRANCHISE_ABBRV)
 
-# window is equivalent to game_span
+
 def init_grid(data: pd.DataFrame, window=10):
+  """
+  Initialize a 32 by 32 adj-mat with cumsum of scores against each opponent
+  """
   grid = empty_grid()
-  data.sort_values(['year', 'week'], inplace=True)
   for i in range(0, window*16):
     row = data.iloc[i]
     grid.loc[row['Home_Team'], row['Away_Team']] += row['Away_Score']
     grid.loc[row['Away_Team'], row['Home_Team']] += row['Home_Score']
   return grid
 
-def update_grid(data, grid, start=0, window=10):
-  # Remove the entries right before the beginning of the window
-  # This is 16 rows -> 1 for each game
+def update_grid(data, grid, start=0, size = 16, window=10):
+  """
+  Remove the entries right before the beginning of the window
+  This is 16 rows -> 1 for each game
+  Updates are in place
+  """
   del_start = start - window * 16
   if del_start < 0:
     raise Exception("Cannot index less than 0 - check start and window bounds")
-  del_end = del_start + 16
+  del_end = del_start + size
   for i in range(del_start, del_end):
     row = data.iloc(i)
+    print(row)
     grid.loc[row['Home_Team'], row['Away_Team']] -= row['Away_Score']
     grid.loc[row['Away_Team'], row['Home_Team']] -= row['Home_Score']
 
@@ -61,22 +67,26 @@ def rank(rank_dict):
     rank = [k for (k, v) in sorted(rank_dict.items(), key=operator.itemgetter(1))]
     return rank
 
-def rolling_grid(data, grid, window=10):
-  
-  start_week = window * 16
-  end_week = len(data)
-  rolling_hits = pd.DataFrame()
-  for i in range(start_week, end_week):
-    G = nx.from_pandas_adjacency(grid, create_using=nx.MultiDiGraph)
-    points_allowed, points_for = nx.hits(G)
-    week = window % 17 if year < 2021 else window % 18
-    update_grid(data=data, grid=grid, start=i)
+def rolling_grid(data, grid, start, size):
+  G = nx.from_pandas_adjacency(grid, create_using=nx.MultiDiGraph)
+  update_grid(data=data, grid=grid, start=start, size=size)
+  points_allowed, points_for = nx.hits(G)
+  return points_allowed, points_for
 
 
 def hits_test():
   # clean(2022, 6, game_span=1)
   data: pd.DataFrame = pd.read_csv(read_path)
+  data.sort_values(['year', 'week'], inplace=True)
   points_grid = init_grid(data)
+  rolling_hits = pd.DataFrame()
+
+  start = 160
+  for k, v in data.groupby(['year', 'week']):
+    size = len(v)
+    p_a, p_f = rolling_grid(data, points_grid, start, size)
+    start += size
+    print(p_a, p_f)
   
 
 
