069d64c7cb186fed80d183136bf580d094c8ea65
Ryan McIntire <ryanpmcintire@gmail.com>
2022-10-27 12:28:37 -0400

    qwdqwdqwd

--- modules/machines/learner_methods.py
+++ modules/machines/learner_methods.py
@@ -12,38 +12,58 @@ x_cols = [
     'week',
     'Home_Fav',
     'Home_Vegas_Spread',
-    'Trail_Home_Score',
-    'Trail_Away_Score',
-    'Home_Allowed',
-    'Away_Allowed',
-    'Home_TO',
-    'Away_TO',
-    'Home_FTO',
-    'Away_FTO',
-    'Home_Pass_Eff',
-    'Away_Pass_Eff',
-    'Home_Pass_Def',
-    'Away_Pass_Def',
+    # 'Trail_Home_Score',
+    # 'Trail_Away_Score',
+    # 'Home_Allowed',
+    # 'Away_Allowed',
+    # 'Home_TO',
+    # 'Away_TO',
+    # 'Home_FTO',
+    # 'Away_FTO',
+    # 'Home_Pass_Eff',
+    # 'Away_Pass_Eff',
+    # 'Home_Pass_Def',
+    # 'Away_Pass_Def',
     'Home_Rush_Eff',
     'Away_Rush_Eff',
     'Home_Rush_Def',
     'Away_Rush_Def',
-    'Home_Pen_Yds',
-    'Away_Pen_Yds',
-    'Home_Pen_Yds_Agg',
-    'Away_Pen_Yds_Agg',
-    'Home_Third_Eff',
-    'Away_Third_Eff',
-    'Home_Third_Def',
-    'Away_Third_Def',
-    'Home_Fourth_Eff',
-    'Away_Fourth_Eff',
-    'Home_Fourth_Def',
-    'Away_Fourth_Def',
+    # 'Home_Pen_Yds',
+    # 'Away_Pen_Yds',
+    # 'Home_Pen_Yds_Agg',
+    # 'Away_Pen_Yds_Agg',
+    # 'Home_Third_Eff',
+    # 'Away_Third_Eff',
+    # 'Home_Third_Def',
+    # 'Away_Third_Def',
+    # 'Home_Fourth_Eff',
+    # 'Away_Fourth_Eff',
+    # 'Home_Fourth_Def',
+    # 'Away_Fourth_Def',
     'wonLast',
     'lostLast',
-    'lostLastAsFav',
-    'wonLastAsDog',
+    # 'lostLastAsFav',
+    # 'wonLastAsDog',
+    'Home_Off_Rank',
+    'Home_Def_Rank',
+    'Away_Off_Rank',
+    'Away_Def_Rank'
+]
+
+x_cols_xgb = [
+    'week',
+    'Home_Fav',
+    'Home_Vegas_Spread',
+    'Home_Rush_Eff',
+    'Away_Rush_Eff',
+    'Home_Rush_Def',
+    'Away_Rush_Def',
+    'wonLast',
+    'lostLast',
+    'Home_Off_Rank',
+    'Home_Def_Rank',
+    'Away_Off_Rank',
+    'Away_Def_Rank'
 ]
 
 def print_eval(X_test, y_pred, y_test):
@@ -69,11 +89,11 @@ def read_data(_read_path=read_path):
     data: df = pd.read_csv(_read_path)
     data = data.sort_values(['year', 'week'])
     # first 10 games for each team blank (10 x 32) but divide by 2 because we only record 1 instance of each matchup
-    data = data.iloc[160:]
+    data = data.iloc[146:]
     return data
 
-def get_splits(year, week=None, test_size = 0.2, stratify='week'):
-    data = read_data(read_path)
+def get_splits(year, week=None, test_size = 0.2, stratify='week', _read_path=read_path):
+    data = read_data(_read_path)
 
     train = data[data['year'] < year]
     if week:
--- modules/machines/xgboost_test.py
+++ modules/machines/xgboost_test.py
@@ -5,11 +5,11 @@ import xgboost as xgb
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from pathlib import Path
-from machines.learner_methods import print_eval, get_splits, read_data, plot, read_path, x_cols
+from machines.learner_methods import print_eval, get_splits, read_data, plot, x_cols_xgb
 import pandas as pd
 from sklearn.metrics import mean_squared_error, explained_variance_score, r2_score, mean_absolute_error, median_absolute_error, mean_absolute_percentage_error
 from cleaner import clean
-
+read_path = './cleaned_with_rank.csv'
 
 
 def xgb_learner(year=2022, week=6):
@@ -25,23 +25,26 @@ def xgb_learner(year=2022, week=6):
     plot(X_test, y_pred, y_test)
 
 def predict(year, week):
-    xgb_model = xgb.XGBRegressor(n_estimators=25000, objective='reg:squarederror', subsample=0.6, learning_rate=0.00017, tree_method="approx", max_depth=1, booster='gbtree', eval_metric='rmse', random_state=88)
-    X_train, X_test, y_train, y_test = get_splits(year, week, test_size=.5, stratify='Home_Fav')
+    xgb_model = xgb.XGBRegressor(n_estimators=3500, objective='reg:pseudohubererror', reg_lambda =.01, reg_alpha=0.07, learning_rate=0.002, booster='gblinear', eval_metric='rmse', random_state=88)
+    X_train, X_test, y_train, y_test = get_splits(year, week, test_size=.4, stratify='Home_Fav', _read_path=read_path)
     data = read_data(read_path)
 
     predictionResultPath = Path(f'predictions/xgboost_week_{week}.csv')
     predictionResultPath.parent.mkdir(parents=True, exist_ok=True)
 
     estimator = xgb_model.fit(X_train, y_train)
+    # feature_impportance = dict(zip(x_cols, estimator.feature_importances_))
+    # print(feature_impportance)
+    print(estimator.feature_importances_)
     y_pred = estimator.predict(X_test)
     print_eval(X_test, y_pred, y_test)
     
     train = data[data['year'] < year]
-    X_train = train[x_cols]
+    X_train = train[x_cols_xgb]
     y_train = train['Home_Actual_Spread']
 
     test = data[data['year'] > year - 1][data['week'] == week]
-    X_test = test[x_cols]
+    X_test = test[x_cols_xgb]
 
     estimator.fit(X_train, y_train)
     y_test_pred = pd.DataFrame(estimator.predict(X_test), columns=['Predicted Spread'])
